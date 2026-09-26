e38f64ff3ca8bc53b3bb59dcd7c2574d6223e0fe
Spidey6978 <veergopani55@gmail.com>
2026-09-26 23:43:32 +0530

    chore: remove unused Noise import

--- src/components/ctrl-freak/SystemCore.tsx
+++ src/components/ctrl-freak/SystemCore.tsx
@@ -6,7 +6,7 @@ import { GLTFExporter } from 'three-stdlib';
 import { MotionValue } from 'framer-motion';
 import { ControlChamber } from './ControlChamber';
 import { useCtrlFreakStore } from '../../store/useCtrlFreakStore';
-import { EffectComposer, Bloom, Vignette, Noise } from '@react-three/postprocessing';
+import { EffectComposer, Bloom, Vignette } from '@react-three/postprocessing';
 
 const AmbientDust = () => {
   const pointsRef = useRef<THREE.Points>(null);
